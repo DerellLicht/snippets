e035933944963182e9967a5830e04fc809d7ef25
Derell Licht <derelict@comcast.net>
2021-08-08 09:33:36 -0700

    code cleanup

--- prime32.cpp
+++ prime32.cpp
@@ -20,6 +20,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//lint -esym(818, argv)  could be declared as pointing to const
+//lint -esym(534, gets)  Ignoring return value of function
+
 char tempstr[81] ;
 
 enum {
@@ -31,7 +34,8 @@ D_INTERACTIVE
 int main(int argc, char** argv)
 {
    unsigned __int64 givennbr, nbrleft, nextodd;
-   int power, prime ;
+   int power ;
+   bool prime ;
    int next = 1 ;
    int display = (argc > 1) ? D_STANDARD : D_INTERACTIVE ;
 
@@ -41,42 +45,45 @@ int main(int argc, char** argv)
    puts("then displays either the number or its factors.");
 
    do {  /* Repeat main program until entry = 0 */
-      prime = 1 ;  givennbr = power = 0L ;
-      switch (display)
-         {
-         case D_INTERACTIVE:
-            puts("");
-            printf("Enter a number (0 quits): ");
-            gets(tempstr) ;
-            givennbr = strtoull(tempstr, NULL, 10) ;
-            if (givennbr == 0)
-               return 0;
-            break;
-         case D_STANDARD:
-            if (argv[next] == NULL)
-               exit(0) ;
-            puts("****************************************************\n");
-            givennbr = strtoull(argv[next++], NULL, 10) ;
-            if (givennbr == 0)
-               return 0;
-            break;
-         }
+      prime = true ;  givennbr = power = 0L ;
+      switch (display) {
+      case D_INTERACTIVE:
+         puts("");
+         printf("Enter a number (0 quits): ");
+         gets(tempstr) ;   //lint !e421  dangerous function
+         givennbr = strtoull(tempstr, NULL, 10) ;
+         if (givennbr == 0)
+            return 0;
+         break;
+      // case D_STANDARD:
+      default:
+         if (argv[next] == NULL)
+            exit(0) ;
+         puts("****************************************************\n");
+         givennbr = strtoull(argv[next++], NULL, 10) ;
+         if (givennbr == 0)
+            return 0;
+         break;
+      }
 
       //****************************************************
       //  factor out powers of 2
       //****************************************************
       nbrleft = givennbr;
-      if (!(nbrleft & 1)) {
-         while (!(nbrleft & 1)) {      /* while nbrleft mod 2 = 0  */
+      if ((nbrleft & 1) == 0) {
+         while ((nbrleft & 1) == 0) {      /* while nbrleft mod 2 = 0  */
             power++ ;
             // nbrleft /= 2 ;
             nbrleft >>= 1 ;
          }
-         if (prime) {
-            printf("The factors of %I64u are:\n\n", givennbr) ;
+         // if (prime) {
+            printf("The factors of %I64u are:\n\n", givennbr) ;   //lint !e539 Did not expect positive indentation
             printf("          Factor                       Power \n");
-            prime = 0 ;
-         }
+            prime = false ;   //  keep this from displaying again later
+         // }
+         //  Okay, I get it... when the preceding while loop is done,
+         //  if original number was multiple of 2,
+         //  then nbrleft will equal 1, that being the power sign
          printf("   %20I64u       %20I64u\n", (__int64) 2, (__int64) power) ;
       }
 
@@ -94,7 +101,7 @@ int main(int argc, char** argv)
             if (prime) {
                printf("The factors of %I64u are:\n\n", givennbr) ;
                printf("          Factor                       Power \n");
-               prime = 0 ;
+               prime = false ;
             }
             printf("   %20I64u       %20I64u\n", nextodd, (__int64) power) ;
          }  /* IF nbrleft%nextodd    */
@@ -104,7 +111,7 @@ int main(int argc, char** argv)
       //****************************************************
       //  check for remaining value
       //****************************************************
-      if (nbrleft != 1) {
+      if (nbrleft != 1) {  //  this will only occur if orig number was power of 2
          if (prime)
             printf("%I64u is a prime number\n", givennbr) ; 
          else
