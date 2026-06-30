93e210dc2a3d41caad7dc0e3d229934881b56276
Derell Licht <derelict@comcast.net>
2026-06-30 14:03:07 -0700

    add single-file build/check script

--- do_one_file.cmd
+++ do_one_file.cmd
@@ -1,28 +1,31 @@
 ::  This will be an example file that demonstrates flow-control options
-::  Here is a page with An A-Z Index of Windows CMD commands
+::  To see An A-Z Index of Windows CMD commands, visit:
 ::  https://ss64.com/nt/
 
-@if /I "%~1"=="" goto :usage
-@if /I "%~2"=="--build" goto :build
-@if /I "%~2"=="--check" goto :check
+@if /I "%~1"=="--build" goto :build
+@if /I "%~1"=="--check" goto :check
+@if /I "%~2"=="" goto :usage
 
 :usage
-   echo USAGE:
-   echo    choices.cmd <filename> [--build] [--check]
-   echo.
-   echo ARGUMENTS
-   echo       <filename>  [mandatory]
-   echo       --build - run build command on <filename>
-   echo       --check - run clang-tidy against <filename>.cpp
-   echo.
-   echo       Either --build or --check are required
-   goto :eof
+   @echo USAGE:
+   @echo     do_one_file [--build] [--check] ^<filename^>
+   @echo.
+   @echo ARGUMENTS
+   @echo    --build - run build command on ^<filename^>
+   @echo    --check - run clang-tidy against ^<filename^>.cpp
+   @echo.
+   @echo    Either --build or --check are required
+   @echo    ^<filename^>  [mandatory, filename does *not* include extension here]
+   @echo.
+   @echo    Example: 
+   @echo    do_one_file --build read_files
+   @goto :eof
 
 :build
-   g++ -Wall -O2 %1.cpp -o %1.exe
+   g++ -Wall -O2 %2.cpp -o %2.exe
    @goto :eof
 
 :check
-   clang-tidy %1.cpp -- 
+   clang-tidy %2.cpp -- 
    @goto :eof
 
