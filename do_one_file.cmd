f03ac09f4150a50bfd100263a6f4fb8f4c4ca8d0
Derell Licht <derelict@comcast.net>
2026-08-23 10:11:18 -0700

    code cleanup

--- do_one_file.cmd
+++ do_one_file.cmd
@@ -3,22 +3,22 @@
 ::  https://ss64.com/nt/
 
 @if /I "%~2"=="" goto :usage
-@if /I "%~1"=="--build" goto :build
-@if /I "%~1"=="--check" goto :check
+@if /I "%~1"=="build" goto :build
+@if /I "%~1"=="check" goto :check
 
 :usage
    @echo USAGE:
-   @echo     do_one_file [--build] [--check] ^<filename^>
+   @echo     do_one_file [build] [check] ^<filename^>
    @echo.
    @echo ARGUMENTS
-   @echo    --build - run build command on ^<filename^>
-   @echo    --check - run clang-tidy against ^<filename^>.cpp
+   @echo    build - run build command on ^<filename^>
+   @echo    check - run clang-tidy against ^<filename^>.cpp
    @echo.
-   @echo    Either --build or --check are required
+   @echo    Either build or check are required
    @echo    ^<filename^>  [mandatory; filename does *not* include extension here]
    @echo.
    @echo    Example: 
-   @echo    do_one_file --build read_files
+   @echo    do_one_file build read_files
    @goto :eof
 
 :build
